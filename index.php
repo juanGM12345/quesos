ee1f895c54b300f77e410070169baf37ef70ef92
JuanAGM <soraka.jagm@gmail.com>
2023-02-14 00:00:31 -0600

    quesos
    quesos

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/carrucel.css" rel="stylesheet">
+    <link href="css/productos.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+ <script src="js/carrucel.js" type="text/javascript"></script>
+    <title>Document</title>
+</head>
+<body style="width:100%;height: 100%;margin:0px;padding:0px;">
+    <div style="width:100%;height:80px; position:fixed;float:left;background:#000661d8;z-index:10;display:flex;">
+    <img src="img/image.png" style="height:40px;margin:auto;margin-left:20px;margin-right:0px;">
+    <p style="margin:auto;margin-left:10px;color:white; font-family: 'Bebas Neue', cursive;font-size:20px;">Comercializadora Toledano</p>
+    <div style="margin:auto;margin-right:20px;">
+        <a href="#inicio" style="text-decoration:none;color:white;font-family: 'Heebo', sans-serif;">INICIO</a>
+        <a href="#nosotros" style="text-decoration:none;margin-left:20px;color:white;font-family: 'Heebo', sans-serif;">NOSOTROS</a>
+        <a href="#producto" style="text-decoration:none;margin-left:20px;color:white;font-family: 'Heebo', sans-serif;">PRODUCTOS</a>
+        <a href="#Contacto" style="text-decoration:none;margin-left:20px;color:white;font-family: 'Heebo', sans-serif;">CONTACTO</a>
+    </div>
+    </div>
+
+    <div id="inicio" style="width:100%;height:auto; float:left;z-index:0;margin-left:0px;display:flex;flex-wrap: wrap;margin-top:80px;padding-bottom:140px;padding-top:60px;">
+    <h3 style="margin:auto;color:black; font-family: 'Bebas Neue', cursive;font-size:3em;text-align:center;">Comercializadora Toledano</h3>
+    <img src="img/inicio-4 - copia 2.png" style="margin:auto;min-width:320px;max-width:500px;">
+    </div>
+
+    <div id="nosotros" style="width:100%;height:auto; float:left;z-index:0;margin-left:0px;display:flex;flex-wrap: wrap;background:#F2F2F2;padding-top:60px;">
+
+    <div style="margin:auto;max-width:980px;min-width:320px;">
+
+    <div style="margin:auto;max-width:980px;min-width:320px;loat:left;">
+                <div style="border-radius:100%;width:300px;height:280px;float:left;margin:10px;">
+                <h3 id="contenedorContacto" style="MARGIN-BOTTOM:0PX;font-family: 'Bebas Neue', cursive;font-size:30px;">
+                MISIÓN
+                </h3>
+                <P style="font-family: 'Heebo', sans-serif;padding:0px;margin:0px;text-align: justify;">
+                Satisfacer las necesidades de nuestros clientes brindando atención personalizada, ofreciendo productos de alta calidad,
+                 apoyando de esta forma su crecimiento en el mercado local.
+                </P>
+                </div>
+
+                <div style="background:white;border-radius:100%;width:340px;height:auto;float:left;margin-top:160px;">
+                <img src="img/vino.png" style="width:300px;">
+                </div>
+
+                <div style="border-radius:100%;width:300px;height:280px;float:left;margin:10px;">
+                <h3 id="contenedorContacto" style="MARGIN-BOTTOM:0PX;font-family: 'Bebas Neue', cursive;font-size:30px;text-align: right;">
+                VISIÓN
+                </h3>
+                <P style="font-family: 'Heebo', sans-serif;padding:0px;margin:0px;text-align: justify;">
+                Consolidarnos en los próximos 5 años como una empresa comercializadora reconocida en los tres estados de la península 
+                de Yucatán por proporcionar a sus clientes un servicio eficiente y competitivo.
+                </div>
+    </div>
+
+    <div style="margin:auto;max-width:920px;min-width:320px;float:left;margin-top:-50px;">
+
+
+                <div style="border-radius:100%;width:300px;height:280px;float:left;margin:10px;margin-top:0px;">
+                <h3 id="contenedorContacto" style="MARGIN-BOTTOM:0PX;font-family: 'Bebas Neue', cursive;font-size:30px;">
+                VALORES
+                </h3>
+                <P style="font-family: 'Heebo', sans-serif;padding:0px;margin:0px;text-align: justify;">
+                &#8226; Atención al cliente.<br>
+                &#8226; Servicio.<br>
+                &#8226; Compromiso.<br>
+                &#8226; Responsabilidad.<br>
+                &#8226; Trabajo en equipo.<br>
+                &#8226; Responsabilidad Social.
+
+                </P>
+                </div>
+                <div style="border-radius:100%;width:280px;height:1px;float:left;">
+               
+                </div>
+
+                <div style="border-radius:100%;width:300px;height:280px;float:left;margin:10px;">
+                <h3 id="contenedorContacto" style="MARGIN-BOTTOM:0PX;font-family: 'Bebas Neue', cursive;font-size:30px;text-align: right;">
+                Fundación
+                </h3>
+                <P style="font-family: 'Heebo', sans-serif;padding:0px;margin:0px;text-align: justify;">
+                En 2010 se funda Comercializadora Toledano S.A. DE C.V., estableciendo su primera sucursal en el Fraccionamiento Herradura 
+                IV en el desarrollo urbano «Ciudad Caucel» en la parte poniente de la Ciudad de Mérida. 
+                </P>
+                </div>
+    </div>
+
+
+    </div>
+    </div>
+<!--origen-->
+<div id="nosotros" style="width:100%;height:auto; float:left;z-index:0;margin:0px;margin-left:0px;display:flex;flex-wrap: wrap;padding-top:0px;">
+<img src="img/pngegg (15).png" style="width:240px;position:absolute;">
+<img src="img/pngegg (14).png" style="width:240px;position:absolute;margin-top:650px;right:0px;">
+<div style="min-width:300px;max-width:500px;margin:auto;position:relative;height:auto;margin-top:80px;margin-bottom:80px;">
+    <div style="border-radius:100%;min-width:300px;max-width:500px;height:auto;float:left;">
+                    <h3 id="contenedorContacto" style="MARGIN-BOTTOM:0PX;font-family: 'Bebas Neue', cursive;font-size:30px;">
+                    2010
+                    </h3>
+                    <P style="font-family: 'Heebo', sans-serif;padding:0px;margin:0px;text-align: justify;">
+                    En 2010 se funda Comercializadora Toledano S.A. DE C.V., estableciendo su primera sucursal en el Fraccionamiento Herradura IV 
+                    en el desarrollo urbano «Ciudad Caucel» en la parte poniente de la Ciudad de Mérida. Con la fundación de dicha sucursal inicia 
+                    la atención de las necesidades de las tiendas de abarrotes de la ciudad de Mérida y zonas aledañas. 
+                    </P>
+    </div>
+
+    <div style="border-radius:100%;min-width:300px;max-width:500px;height:auto;float:left;">
+                    <h3 id="contenedorContacto" style="MARGIN-BOTTOM:0PX;font-family: 'Bebas Neue', cursive;font-size:30px;">
+                    2021
+                    </h3>
+                    <P style="font-family: 'Heebo', sans-serif;padding:0px;margin:0px;text-align: justify;">
+                    En el año 2021, con el éxito que significó la primera sucursal entre los comerciantes de la ciudad de Mérida y zonas aledañas, 
+                    Comercializadora Toledano S.A. DE C.V decide ampliar sus horizontes, y con esto, se presenta la oportunidad de establecer una 
+                    segunda sucursal, ahora en la ciudad de Campeche en el estado de Campeche, donde tuvo gran recibimiento entre los comerciantes 
+                    de abarrotes locales.
+                    </P>
+    </div>
+    <div style="border-radius:100%;min-width:300px;max-width:500px;height:auto;float:left;">
+                    <h3 id="contenedorContacto" style="MARGIN-BOTTOM:0PX;font-family: 'Bebas Neue', cursive;font-size:30px;">
+                    2021
+                    </h3>
+                    <P style="font-family: 'Heebo', sans-serif;padding:0px;margin:0px;text-align: justify;">
+                    En el año 2021, con el éxito que significó la primera sucursal entre los comerciantes de la ciudad de Mérida y zonas aledañas, 
+                    Comercializadora Toledano S.A. DE C.V decide ampliar sus horizontes, y con esto, se presenta la oportunidad de establecer una 
+                    segunda sucursal, ahora en la ciudad de Campeche en el estado de Campeche, donde tuvo gran recibimiento entre los comerciantes 
+                    de abarrotes locales.
+                    </P>
+    </div>
+
+
+</div>
+<img src="img/pngegg (9).png" style="width:240px;margin:auto;position:relative;margin-left:0px;margin-right:0px;">
+<div style="min-width:300px;max-width:500px;margin:auto;position:relative;height:auto;">
+
+    <div style="border-radius:100%;min-width:300px;max-width:500px;height:auto;float:left;">
+                    <h3 id="contenedorContacto" style="MARGIN-BOTTOM:0PX;font-family: 'Bebas Neue', cursive;font-size:30px;">
+                    2021
+                    </h3>
+                    <P style="font-family: 'Heebo', sans-serif;padding:0px;margin:0px;text-align: justify;">
+                    A tan solo algunos meses de haberse fundado la segunda sucursal, inician operaciones de una nueva sucursal, la tercera en orden 
+                    cronológico, establecida en la ciudad de Cancún en el estado de Quintana Roo. Con la fundación de esta última, Comercializadora 
+                    Toledano S.A. DE C.V., se consolida desde ese momento como una empresa comercializadora con presencia en los tres estados de la 
+                    península de Yucatán.
+                    </P>
+    </div>
+    <div style="border-radius:100%;min-width:300px;max-width:500px;height:auto;float:left;">
+                    <h3 id="contenedorContacto" style="MARGIN-BOTTOM:0PX;font-family: 'Bebas Neue', cursive;font-size:30px;">
+                    2022
+                    </h3>
+                    <P style="font-family: 'Heebo', sans-serif;padding:0px;margin:0px;text-align: justify;">
+                    En el año 2022, tras doce años de haberse establecido la primera sucursal en el Fraccionamiento Herradura IV en el desarrollo 
+                    urbano «Ciudad Caucel» en la ciudad de Mérida, por razones comerciales, se decide la mudanza de la sucursal hacia el suroeste 
+                    del estado, específicamente al municipio de Umán, y se instala en la colonia ampliación ciudad industrial, zona reconocida por 
+                    su amplia afluencia comercial, donde actualmente continúa atendiendo las necesidades de sus clientes.
+                    </P>
+    </div>
+</div>
+</div>
+
+<!--productos-->
+
+<div id="producto" style="margin-top:0px;width: 100%;height: auto;padding-top:50px;padding-bottom:100px;display: flex;flex-wrap: wrap;overflow-x: none;overflow-y: none; float: left;background:#F2F2F2;">
+<h3 id="contenedorContacto" style="width:70%;height:auto;margin-left:15%;margin-bottom:50px;font-family: 'Bebas Neue', cursive;font-size:30px;">
+Productos
+</h3>
+<img src="img/productos.png" style="margin:auto;min-width:320px;max-width:420px;">
+       
+        <div id="contenedor-productos">
+          
+            <div id="item-producto" style="">
+                <img src="img/producto-1.png" style="width: 150px;
+    height: auto;margin-left:25px;">
+   
+                <div id="producto-nombre" style="font-family: 'Heebo', sans-serif;">nombre</div>
+            </div>
+       
+            
+            <div id="item-producto" style="">
+                <img src="img/producto-1.png" style="width: 150px;
+    height: auto;margin-left:25px;">
+                <div id="producto-nombre" style="font-family: 'Heebo', sans-serif;">nombre</div>
+            </div>
+       
+          
+            <div id="item-producto" style="">
+                <img src="img/producto-2.png" style="width: 150px;
+    height: auto;margin-left:25px;">
+                <div id="producto-nombre" style="font-family: 'Heebo', sans-serif;">nombre</div>
+            </div>
+
+            <div id="item-producto" style="">
+                <img src="img/producto-3.png" style="width: 150px;
+    height: auto;margin-left:25px;margin-top:50px;">
+                <div id="producto-nombre" style="font-family: 'Heebo', sans-serif;">nombre</div>
+            </div>
+
+            <div id="item-producto" style="">
+                <img src="img/producto-1.png" style="width: 150px;
+    height: auto;margin-left:25px;">
+                <div id="producto-nombre" style="font-family: 'Heebo', sans-serif;">nombre</div>
+            </div>
+            <div id="Opciones-GP">
+                <a href="productos.html" style="text-decoration: none;margin: auto;margin-right: 5%;"><button id="mas-p" style="font-family: 'Heebo', sans-serif;">Ver más 
+                 <svg xmlns="http://www.w3.org/2000/svg" width="" height="20" fill="white" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                 </svg>
+           </button></a>
+            </div>
+        </div>
+        
+</div>
+<!--formulario de contacto-->
+
+<div id="Contacto" style="display:flex;flex-wrap: wrap;margin-top:70px;width:100%;">
+<div id="contenedorContacto" style="width:70%;height:1px;background:#CFCFCF;margin-left:15%;margin-top:70px;">
+</div>
+<form action="" style="width:400px;margin:auto;border-radius:5px;">
+    <h3 style="width:80%;margin-left:10%;float:left;text-align: center;font-family: 'Bebas Neue', cursive;font-size:30px;">
+        Contactanos
+    </h3>
+    <input type="text" style="width:80%;margin-left:10%;float:left;margin-top:10px;background-color: #F2F2F2;border:none;padding:5px;">
+<input type="text" style="width:80%;margin-left:10%;float:left;margin-top:10px;background-color: #F2F2F2;border:none;padding:5px;">
+<input type="text" style="width:80%;margin-left:10%;float:left;margin-top:10px;background-color: #F2F2F2;border:none;padding:5px;">
+<textarea name="" id="" cols="30" rows="10" style="width:80%;margin-left:10%;float:left;margin-top:10px;margin-bottom:10px;background-color: #F2F2F2;border:none;padding:5px;"></textarea>
+<div style="display:flex;width:100%;margin-bottom:30px;">
+<button style="margin:auto;background:#009903;font-family: 'Heebo', sans-serif;color:white;padding:10px;border:none;border-radius:5px;">Enviar</button>
+</div>
+</form>
+<img src="img/contacto-2.png" style="margin:auto;max-width:25%;min-width:320px;margin-left:10px;">
+</div>
+<!--fin de productos-->
+<div style="margin-top:70px;width: 100%;height: auto; margin-left:0%;display: flex; flex-wrap: wrap;margin-right:0%;overflow-x: none;overflow-y: none; float: left;background: #000661d8;">
+
+<div style="float: left;max-width: 700px;min-width: 320px;height: auto;color: white;margin-top: 60px;margin-bottom: 30px;margin-left: 25px;font-family: 'Heebo', sans-serif;line-height : 18px;">
+Comercializadora Toledano
+    <br>
+    <br>
+    ventas@comercializadoratoledano.com <br>
+    Teléfono: 999 316 8638
+<br>
+
+Calle 19 por 42 y 46 #300 Ampliacion, Cd Industrial, 97302 Umán, Yuc.
+<br>
+
+Horario: lunes – viernes de 7:00 -19:00 hrs. Sábado 7:00 – 15:00 hrs.
+
+</div>
+    
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.453070915855!2d-89.66649489999999!3d20.9744681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f56730b4cfc55b9%3A0xcbf933470b64b9fa!2sSistemas%20integrales%20de%20comunicaci%C3%B3n%20y%20servicios!5e0!3m2!1ses!2smx!4v1654661762387!5m2!1ses!2smx" width="400" height="250" style="border:0;margin-right: 25px;float: left;margin-left: auto;margin-top:20px;margin-bottom:20px;"></iframe>
+
+    
+ 
+</div>
+</body>
+</html>
